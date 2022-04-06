3c9079217136355641e674bdf31b1ac870990b35
phw-plain <s2028@e-mirim.hs.kr>
2022-04-06 10:27:27 +0900

    키 입력 기능 및 정답 확인 기능 업데이트

--- Themehangman/src/Playing.java
+++ Themehangman/src/Playing.java
@@ -1,13 +1,25 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.text.Normalizer;
 
 import javax.swing.*;
 import javax.swing.event.*;
 
 public class Playing extends ProSetting {
 	public MyPanel panel;
-
-	char ch[] = {
+	public JButton keyboard[];
+	public JButton text[];
+	
+	public String que;
+	char[][] que_answer = new char[][] {
+		{'ㅂ', 'ㅏ'},
+		{'ㄷ', 'ㅏ'},
+		{'ㅋ', 'ㅗ'},
+		{'ㄱ', 'ㄱ', 'ㅣ'},
+		{'ㄹ', 'ㅣ'}
+	};
+	 
+	public char	ch[] = {
 			'ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ',
 			'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ',
 			'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ',
@@ -22,10 +34,46 @@ public class Playing extends ProSetting {
 		
 	    panel = new MyPanel();
 	    
+	    // Screen
+	    // replace이용해서 배열 stage 이미지 만들기 350x300px 로 만들기
+		ImageIcon stage = new ImageIcon("src/stage/stage1.png");
+	   
+		JButton screen = new JButton(stage);
+		screen.setContentAreaFilled(false);
+		screen.setBorderPainted(false);
+		screen.setFocusPainted(false);
+		screen.setBorder(BorderFactory.createEmptyBorder(50, 0, 0, 0));
+		
+		// Question
+	    JPanel question = new JPanel(new GridLayout(2, 5));
+	    question.setBackground(background);
+	    question.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+	    que = "바다코끼리";
+	    text = new JButton[10];
+	    for(int i=0; i<que.length(); i++) {
+	    	text[i] = new JButton();
+	    	text[i].setText(" ");
+	    	text[i].setFont(font1);
+	    	text[i].setContentAreaFilled(false);
+	    	text[i].setBorderPainted(false);
+	    	text[i].setFocusPainted(false);
+	    	question.add(text[i]);
+	    }
+	    for(int i=que.length(); i<que.length()*2; i++) {
+	    	text[i] = new JButton();
+	    	text[i].setText("─");
+	    	text[i].setFont(font1);
+	    	text[i].setContentAreaFilled(false);
+	    	text[i].setBorderPainted(false);
+	    	text[i].setFocusPainted(false);
+	    	question.add(text[i]);
+	    }
+	    
+	    // Typing
 	    JPanel typing = new JPanel(new GridLayout(5, 5, 5, 5));
 	    typing.setBackground(background);
-	    typing.setBorder(BorderFactory.createEmptyBorder(100, 0, 0, 0));
-	    JButton keyboard[] = new JButton[ch.length];
+	    typing.setBorder(BorderFactory.createEmptyBorder(15, 50, 10, 50));
+	    keyboard = new JButton[ch.length];
 	    for(int i=0; i<ch.length; i++) {
 	    	if(i == 14) {
 	    		// blank
@@ -42,21 +90,67 @@ public class Playing extends ProSetting {
 	    	keyboard[i].setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		    typing.add(keyboard[i]);
 	    }
+	    for(int i=0; i<ch.length; i++) {
+	    	keyevent(i);
+	    }
 	    
-	    JPanel question = new JPanel(new GridLayout(1, 5));
-	    JLabel text[];
+	    // Answer
+	    JPanel answer = new JPanel();
+	    answer.setBackground(background);
+	    answer.setBorder(BorderFactory.createEmptyBorder(15, 0, 0, 0));
+	    JTextField input = new JTextField("정답입력", 10);
+	    input.setFont(font1);
 	    
-	    // replace이용해서 배열 stage 이미지 만들기 350x300px 로 만들기
-		ImageIcon stage = new ImageIcon("src/stage/stage1.png");
-	   
-		JButton screen = new JButton(stage);
-		screen.setContentAreaFilled(false);
-		screen.setBorderPainted(false);
-		screen.setFocusPainted(false);
-		screen.setBorder(BorderFactory.createEmptyBorder(50, 0, 0, 0));
+	    input.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("텍스트 입력 : " + input.getText() + ", " + que);
+				if(input.getText().equals(que)) {
+					JOptionPane.showMessageDialog(null
+							, "정답!!"
+							, "ThemeHangman"
+							, JOptionPane.PLAIN_MESSAGE
+					);
+				}
+			}
+		});
+	    
+	    answer.add(input);
 
 	    panel.add(screen);
+	    panel.add(question);
 	    panel.add(typing);
+	    panel.add(answer);
+	}
+
+	private void keyevent(int idx) {
+		keyboard[idx].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// 버튼 클릭 효과음 추가하기
+				System.out.println(keyboard[idx].getText() + "키 입력!");
+				answer(idx);
+			}
+		});
+	}
+	
+	private void answer(int idx) {
+		for(int i=0; i<que_answer.length; i++) {
+			for(int j=0; j<que_answer[i].length; j++) {
+				if(ch[idx] == que_answer[i][j]) {
+					if(text[i].getText() == " ") {
+						text[i].setText(String.valueOf(que_answer[i][j]));
+						que_answer[i][j] = '/';
+					} else if(text[i].getText().charAt(0) > que_answer[i][j]) {
+						String temp = String.valueOf(que_answer[i][j]);
+						temp +=	text[i].getText();
+						text[i].setText(temp);
+						que_answer[i][j] = '/';
+					} else {
+						text[i].setText(text[i].getText() + String.valueOf(que_answer[i][j]));
+						que_answer[i][j] = '/';
+					}
+				}
+			}
+		}
 	}
 
 	public void setVisible(boolean tf) {
